1514fece0d6c3ec732e648460321a0cb64348a60
suzhengtang <1037617576@qq.com>
2019-05-08 22:28:18 +0800

    json数据格式化，在界面上显示

--- my-project/src/views/demo/0425/index.vue
+++ my-project/src/views/demo/0425/index.vue
@@ -4,6 +4,8 @@
     <input type="text" v-model="demoVal">
     <p>{{demoWatch}}</p>
     <button @click="dataChange">点击让树的数据发生变化</button>
+    <br>
+    <textarea v-model="excelData" name="" id="" cols="60" rows="10" placeholder="请导入数据"></textarea>
   </div>
 </template>
 
@@ -15,6 +17,7 @@
         demoVal: "",
         demoWatch: "",
         data2: [],
+        excelData: "",
         selectData: ["专业管理", "问题任务"],
         testData: ["测试试题", "专业设置","添加"],
         message: [
@@ -90,6 +93,7 @@
     },
     created() {
       this.data2 = this.getData(this.message, this.selectData);
+      this.excelData = this.formatJson(this.message)
     },
     methods: {
       getCheckbox(data) {
@@ -117,6 +121,56 @@
       },
       dataChange(){
         this.data2 = this.getData(this.message, this.testData);
+      },
+      formatJson(json, options) {
+          var reg = null, formatted = '', pad = 0, PADDING = '    ';
+          options = options || {};
+          options.newlineAfterColonIfBeforeBraceOrBracket = (options.newlineAfterColonIfBeforeBraceOrBracket === true) ? true : false;
+          options.spaceAfterColon = (options.spaceAfterColon === false) ? false : true;
+          if (typeof json !== 'string') {
+             json = JSON.stringify(json);
+          } else {
+             json = JSON.parse(json);
+             json = JSON.stringify(json);
+          }
+          reg = /([\{\}])/g;
+          json = json.replace(reg, '\r\n$1\r\n');
+          reg = /([\[\]])/g;
+          json = json.replace(reg, '\r\n$1\r\n');
+          reg = /(\,)/g;
+          json = json.replace(reg, '$1\r\n');
+          reg = /(\r\n\r\n)/g;
+          json = json.replace(reg, '\r\n');
+          reg = /\r\n\,/g;
+          json = json.replace(reg, ',');
+          if (!options.newlineAfterColonIfBeforeBraceOrBracket) {
+               reg = /\:\r\n\{/g;
+               json = json.replace(reg, ':{');
+               reg = /\:\r\n\[/g;
+               json = json.replace(reg, ':[');
+          }
+          if (options.spaceAfterColon) {
+               reg = /\:/g;
+               json = json.replace(reg, ':');
+          }
+          (json.split('\r\n')).forEach(function (node, index) {
+                var i = 0, indent = 0, padding = '';
+               if (node.match(/\{$/) || node.match(/\[$/)) {
+                    indent = 1;
+               } else if (node.match(/\}/) || node.match(/\]/)) {
+                    if (pad !== 0) {
+                        pad -= 1;
+                    }
+               } else {
+                   indent = 0;
+               }
+               for (i = 0; i < pad; i++) {
+                    padding += PADDING;
+               }
+               formatted += padding + node + '\r\n';
+               pad += indent;
+           });
+          return formatted;
       }
     },
     watch: {
@@ -128,5 +182,7 @@
 </script>
 
 <style lang="less" scoped>
-
+.demo0425{
+  padding: 20px;
+}
 </style>
